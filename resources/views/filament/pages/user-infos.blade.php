9d50ae10f1dfe25ae30cad67ef3452e3bcc60d23
barrymamadou12345 <dioungol5432@gmail.com>
2024-09-15 04:02:00 +0000

    first commit

---
+++ resources/views/filament/pages/user-infos.blade.php
@@ -0,0 +1,11 @@
+<x-filament-panels::page>
+
+
+  <h1 class="text-3xl">Voici les Informations de l'utilisateur</h1>
+
+  <div class="flex gap-6">
+    <div class="border-2 rounded-xl" style="background-color: orange;width:25%;height:125px;"></div>
+    <div class="border-2 rounded-xl" style="background-color: orange;width:25%;height:125px;"></div>
+  </div>
+
+</x-filament-panels::page>
